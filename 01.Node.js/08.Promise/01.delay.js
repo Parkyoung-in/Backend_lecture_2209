147b616feacb73e9a8173209e948566f749edfd3
YoungIn Park <in951215p@gmail.com>
2022-09-20 09:50:26 +0900

    first

---
+++ 01.Node.js/08.Promise/01.delay.js
@@ -0,0 +1,19 @@
+// Promise
+// Producer(함수)
+function delayP(ms) {
+    return new Promise((resolve, reject) => {       // resolve : 성공했을때 실행되는 함수  // reject : 실패했을때 실행되는 함수
+        try {
+            setTimeout(() => {
+                resolve('성공');        // resolve : 성공했을때 실행되는 callback 함수
+            }, ms);
+        } catch(e) {
+            reject('실패');             // reject : 실패했을때 실행되는 callback 함수
+        }
+    });
+}
+
+// Consumer(함수 불러 쓰는 법)
+delayP(1000)
+    .then((val) => {
+        console.log(val);   // resolve에 해당됨
+    });
